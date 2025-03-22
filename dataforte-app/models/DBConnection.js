e4d4e33145fe3bfb06037e2a0524d37a8e5cb9bb
Uexcel2022 <excellenceudoka@gmail.com>
2025-03-22 18:39:42 +0100

    update

--- dataforte-app/models/DBConnection.js
+++ dataforte-app/models/DBConnection.js
@@ -3,11 +3,19 @@
 
 import mongoose from 'mongoose'
 
-mongoose.connect(process.env.DATABASE,{
-  'authSource': 'admin',
-  'user': process.env.DATABASE_USERNAME,
-  'pass': process.env.DATABASE_PASSWORD,
-}).then(()=> console.log("DB connection successful"))
+// mongoose.connect(process.env.DATABASE,{
+//   'authSource': 'admin',
+//   'user': process.env.DATABASE_USERNAME,
+//   'pass': process.env.DATABASE_PASSWORD,
+// }).then(()=> console.log("DB connection successful"))
+// .catch(err=>{
+//   console.log(err.name,err.message)
+//   console.log('shutting down...')
+//   process.exit(1)
+// })
+
+mongoose.connect(process.env.DATABASE)
+.then(()=> console.log("DB connection successful"))
 .catch(err=>{
   console.log(err.name,err.message)
   console.log('shutting down...')
